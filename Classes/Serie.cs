9e3d737dd8528ab2edca605f151ba15dedc92cd9
Vinicius Fernandes <viniciusnandes@hotmail.com>
2021-10-31 11:51:20 -0300

    Initial commit

---
+++ Classes/Serie.cs
@@ -0,0 +1,49 @@
+
+namespace Series.Classes
+{
+    public class Serie:EntidadeBase
+    {
+        private Genero Genero{get;set;}
+        private string Titulo {get;set;}
+        private string Descricao {get;set;}
+        private int Ano{get;set;}
+        private bool Excluido{get;set;}
+
+
+        public Serie(int id,Genero genero,string titulo,string descricao,int ano){
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano=ano;
+            this.Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: "+ this.Genero +"\n";
+            retorno += "Título: "+ this.Titulo +"\n";
+            retorno += "Descrição: "+ this.Descricao +"\n";
+            retorno += "Ano de Inicio: " +this.Ano;
+            return retorno;
+        }
+
+        public string retornaTitulo(){
+            return this.Titulo;
+        }
+
+        public int retornaId(){
+            return this.Id;
+        }
+        public void Excluir(){
+            this.Excluido=true;
+        }
+
+        public bool retornaExcluido(){
+            return this.Excluido;
+        }
+    }
+
+
+}
---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using Series.Interfaces;
+namespace Series.Classes
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie entidade)
+        {
+            listaSerie[id]=entidade;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            listaSerie.Add(entidade);
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int proximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
